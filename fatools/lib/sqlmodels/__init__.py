b073f8ec441d95e4932212aaa55448c3cf743813
Hidayat Trimarsanto <anto@eijkman.go.id>
2015-06-10 05:37:06 +0700

    added get_allele_dataframe()

--- fatools/lib/sqlmodels/__init__.py
+++ fatools/lib/sqlmodels/__init__.py
@@ -25,8 +25,9 @@ class SQLHandler(object):
         self.engine, self.session = schema.engine_from_file(dbfile)
 
 
-    def initdb(self):
-        schema.Base.metadata.create_all(self.engine)
+    def initdb(self, create_table = True):
+        if create_table:
+            schema.Base.metadata.create_all(self.engine)
         from fatools.lib.sqlmodels.setup import setup
         setup( self.session )
         cout('Database at %s has been initialized.' % self.dbfile)
@@ -38,7 +39,7 @@ class SQLHandler(object):
             cerr('ERR - batch code must be supplied!')
             sys.exit(1)
 
-        return self.Batch.search(batch_code, self.session)
+        return schema.Batch.search(batch_code, self.session)
 
 
     def get_marker(self, marker_code = None):
@@ -47,7 +48,7 @@ class SQLHandler(object):
             cerr('ERR - marker code must be supplied')
             sys.exit(1)
 
-        return self.Marker.search(marker_code, self.session)
+        return schema.Marker.search(marker_code, self.session)
 
 
     def Panel(self):
@@ -55,3 +56,87 @@ class SQLHandler(object):
         p._dbh_session_ = self.session
         return p
 
+    
+    def get_allele_dataframe(self, sample_ids, marker_ids, params):
+        """ return a Pandas dataframe with this columns
+            ( marker_id, sample_id, bin, size, height, assay_id )
+        """
+
+        # params ->
+        # allele_absolute_threshold
+        # allele_relative_threshold
+        # allele_relative_cutoff
+        # peak_type
+
+        q = self.session.query( schema.AlleleSet.sample_id, schema.Channel.assay_id,
+                schema.Allele.marker_id, schema.Allele.bin,
+                schema.Allele.size, schema.Allele.height
+            ).join(schema.Allele).join(schema.Channel)
+
+        q.filter( schema.AlleleSet.sample_id.in_( sample_ids ) )
+
+        if type(peak_type) in [ list, tuple ]:
+            q = q.filter( schema.Allele.type.in_( peak_type  ) )
+        else:
+            q = q.filter( schema.Allele.type == peak_type )
+
+        # we order based on marker_id, sample_id and then descending height
+        q = q.order_by( schema.Allele.marker_id, schema.AlleleSet.sample_id,
+                        schema.Allele.height.desc() )
+
+        print('MARKER IDS:', marker_ids)
+
+        if marker_ids:
+            q = q.filter( schema.AlleleSet.marker_id.in_( marker_ids ) )
+            #q = q.outerjoin( Marker, Allele.marker_id == Marker.id )
+            #q = q.filter( Marker.id.in_( marker_ids ) )
+
+        if allele_absolute_threshold > 0:
+            q = q.filter( schema.Allele.height > allele_absolute_threshold )
+
+        if allele_relative_threshold == 0 and allele_relative_cutoff == 0:
+            df = DataFrame( [ (marker_id, sample_id, value, size, height, assay_id )
+                    for ( sample_id, marker_id, value, size, height, assay_id ) in q ] )
+
+        else:
+
+            alleles = []
+
+            max_height = 0
+            last_marker_id = 0
+            last_sample_id = 0
+            skip_flag = False
+            for sample_id, marker_id, value, size, height, assay_id in q:
+                if sample_id == last_sample_id:
+                    if last_marker_id == marker_id:
+                        if skip_flag:
+                            continue
+                        ratio = height / max_height
+                        if ratio < allele_relative_threshold:
+                            continue
+                        if allele_relative_cutoff > 0 and ratio > allele_relative_cutoff:
+                            # turn off this marker by skipping this sample_id & marker_id
+                            skip_flag = True
+                            # don't forget to remove the latest allele
+                            del alleles[-1]
+                            continue
+                            
+                else:
+                    last_sample_id = sample_id
+                    last_marker_id = marker_id
+                    max_height = height
+                    skip_flag = False
+
+                alleles.append( (marker_id, sample_id, value, size, height, assay_id) )
+
+            df = DataFrame( alleles )
+        
+        if len(df) == 0:
+            return df
+
+        df.columns = ( 'marker_id', 'sample_id', 'value', 'size', 'height', 'assay_id' )
+        return df
+
+
+    def get_sample_ids(self, sample_selector):
+        pass
