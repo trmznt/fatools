c8e88b83e11ccfe3f861963e30612be04786bd75
Hidayat Trimarsanto <anto@eijkman.go.id>
2015-10-27 11:20:02 +0700

    added more code for He

--- fatools/lib/analytics/he.py
+++ fatools/lib/analytics/he.py
@@ -4,6 +4,7 @@ import numpy as np
 from pandas import DataFrame
 from scipy.stats import wilcoxon, kruskal
 
+
 def summarize_he( analytical_sets ):
 
     results = {}
@@ -12,7 +13,7 @@ def summarize_he( analytical_sets ):
     for analytical_set in analytical_sets:
         he[analytical_set.label] = calculate_he(analytical_set.allele_df)
 
-    he_df = DataFrame( results )
+    he_df = DataFrame( he )
     if len(analytical_sets) == 2:
         # use Mann-Whitney / Wilcoxon test
         results['test'] = 'Wilcoxon test (paired)'
@@ -28,6 +29,16 @@ def summarize_he( analytical_sets ):
     return results
 
 
-def calculate_he(allele_df):
-    pass
+def calculate_he(allele_df, adjust=True):
+    """ He is calculated using major allele """
+    marker_he = {}
 
+    dist = allele_df.dominant_df_distribution
+    for (idx, marker_id) in dist:
+        total = sum( n for (v,n) in dist[marker_id] )
+        he = 1.0 - sum( (x/total)**2 for x in dist[marker_id] ) )
+        if adjust and total > 1:
+            he = he *total / (total-1)
+        marker_he[marker_id] = he
+
+    return marker_he
