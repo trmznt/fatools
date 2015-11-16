e22fe1769000dc439e6a9dc5d217b9f238d80ec8
Hidayat Trimarsanto <anto@eijkman.go.id>
2015-11-16 08:58:01 +0700

    added 12 colors from colorbrewer2

--- fatools/lib/analytics/selector.py
+++ fatools/lib/analytics/selector.py
@@ -3,7 +3,9 @@ from itertools import cycle
 from fatools.lib.analytics.sampleset import SampleSet, SampleSetContainer
 from fatools.lib.const import peaktype
 
-colour_list = [ 'r', 'g', 'b' ]
+#colour_list = [ 'r', 'g', 'b' ]
+colour_list = ['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c',
+                '#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#ffff99','#b15928']
 
 
 class Selector(object):
