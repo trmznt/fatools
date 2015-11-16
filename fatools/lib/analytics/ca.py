28e1da36083df5baab754b3fb18053826bb11255
Hidayat Trimarsanto <anto@eijkman.go.id>
2015-11-16 08:59:59 +0700

    fixed working code for MCA and stubs for categorical PCA

--- fatools/lib/analytics/ca.py
+++ fatools/lib/analytics/ca.py
@@ -62,8 +62,13 @@ def plot_pca( pca_result, distance_matrix, pc1, pc2, filename=None ):
                     marker='+',
                     s = 30
         )
-    ax.set_xlabel('PC%d (%.3f%%)' % (pc1 + 1, pca_var[pc1]))
-    ax.set_ylabel('PC%d (%.3f%%)' % (pc2 + 1, pca_var[pc2]))
+
+    if pca_var is not None:
+        ax.set_xlabel('PC%d (%.3f%%)' % (pc1 + 1, pca_var[pc1]))
+        ax.set_ylabel('PC%d (%.3f%%)' % (pc2 + 1, pca_var[pc2]))
+    else:
+        ax.set_xlabel('PC%d' % (pc1 + 1))
+        ax.set_ylabel('PC%d' % (pc2 + 1))
     leg = ax.legend(loc='upper left', scatterpoints=1, fontsize='x-small', fancybox=True,
             bbox_to_anchor=(1,1))
     #leg.get_frame().set_alpha(0.5)
@@ -72,7 +77,47 @@ def plot_pca( pca_result, distance_matrix, pc1, pc2, filename=None ):
     return filename
 
 
-def mca( haplotype_sets, dim = 2 ):
+def mca( distance_matrix, dim = 2 ):
     """ calculate MCA matrix using R's FactorMineR """
 
-    pass
+    # build up haplotype dataframe
+
+    from rpy2 import robjects
+    from rpy2.robjects import pandas2ri
+
+    pandas2ri.activate()
+
+    r_df = pandas2ri.py2ri(distance_matrix.H)
+    robjects.globalenv['haplo_data'] = r_df
+    marker_len = len(distance_matrix.H.columns)
+    arguments = ','.join('as.factor(haplo_data[,%d])' % x
+                    for x in range(1, marker_len + 1))
+    robjects.r('haplo_df <- data.frame(%s)' % arguments)
+    robjects.r('library(FactoMineR)')
+    mca_res = robjects.r('MCA(haplo_df, graph=FALSE)')
+
+    # get the individual coordinate
+    coord = pandas2ri.ri2py(mca_res.rx('ind')[0].rx('coord')[0])
+
+    return (coord, None)
+
+    #mca_result = mca_func(r_df, ** { 'quanti.sup': [1,2,3] })
+    #
+
+    raise RuntimeError
+
+
+def pca( distance_matrix, dim = 2 ):
+    """ calculate PCA matrix using in-house algorithm, treating variables
+        as categorical data
+    """
+
+    D = {}
+
+    for c in distance_matrix.H.columns:
+        D[c] = pandas.get_dummies( distance_matrix.H[c])
+
+    #H = pandas.concat()
+
+    # this function hasn't been finished yet
+    raise NotImplementedError
