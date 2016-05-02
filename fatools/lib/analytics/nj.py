552bc0bf3a0eabc1fde61d7e4ce721c943f91feb
Hidayat Trimarsanto <anto@eijkman.go.id>
2016-05-03 02:14:07 +0700

    added custom tip labeling

--- fatools/lib/analytics/nj.py
+++ fatools/lib/analytics/nj.py
@@ -18,6 +18,7 @@ def plot_nj( distance_matrix, tmp_dir, fmt='pdf', label_callback=None ):
     colors_file = '%s/colors-distance-%s.txt' % (tmp_dir, file_id)
     script_file = '%s/njtree-%s.r' % (tmp_dir, file_id)
     njtree_file = '%s/njtree-%s.%s' % (tmp_dir, file_id, fmt)
+    label_file =  '%s/labels-%s.txt' % (tmp_dir, file_id)
 
     with open(matrix_file, 'w') as out_m, open(colors_file, 'w') as out_c:
 
@@ -27,6 +28,17 @@ def plot_nj( distance_matrix, tmp_dir, fmt='pdf', label_callback=None ):
             out_m.write( '%s\t%s\n' % (str(name), '\t'.join( ['%2.3f' % x for x in vals] ) ))
 
         out_c.write('\n'.join( distance_matrix.C ) )
+
+    if label_callback:
+        with open(label_file, 'w') as labelout:
+            for sample_id in distance_matrix.sample_ids:
+                labelout.write('%d\t%s\n' % (sample_id, label_callback(sample_id)))
+        label_cmd = (   "L<-read.table('%s',sep='\\t',header=F);"
+                        "L[[1]]<-as.character(L[[1]]);"
+                        "L[[2]]<-as.character(L[[2]]);"
+                        "tree$tip.label<-L[[2]][tree$tip.label == L[[1]]]" % label_file )
+    else:
+        label_cmd = ''
 
     with open(script_file, 'w') as scriptout:
         if fmt == 'pdf':
@@ -39,9 +51,10 @@ M <- as.matrix( read.table("%s", sep='\\t', header=T) )
 C <- as.vector( read.table("%s", sep='\\t', header=F, comment.char = '')[,1] )
 tree <- nj( M )
 %s
+%s
 plot(tree, "fan", tip.color = C, font=1, cex=0.7, label.offset = 0.009)
 legend('topright', inset=c(0,0), c(%s), col = c(%s), lty=1, cex=0.85, xpd=T)
-""" % (matrix_file, colors_file, cmd,
+""" % (matrix_file, colors_file, label_cmd, cmd,
         ",".join( '"%s"' % hs.label for (hs,_,_) in distance_matrix.S),
         ",".join( '"%s"' % hs.colour for (hs,_,_) in distance_matrix.S) )
     )
